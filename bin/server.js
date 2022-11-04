ed4e0325a6c1ad3fc0c001433d15ed24d4e3946e
Namzuchu <97218806+rogerzuchu21022019@users.noreply.github.com>
2022-11-04 22:48:56 +0700

    add favicon.ico

--- bin/server.js
+++ bin/server.js
@@ -1,8 +1,8 @@
-const app = require(`../src/app`)
+const app = require(`../src/app`);
 require(`dotenv`).config();
-require(`../src/middlewares/RedisConnection`)
+require(`../src/middlewares/RedisConnection`);
 
-const PORT = process.env.PORT || 3001
-app.listen(PORT,() => {
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
   console.log(`App listening on ${PORT}`);
-})
+});
